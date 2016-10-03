915f5c362d9fc820a40aec0eaa5d4d4f583c32d2
George Frentzel <George@Georges-MacBook-Pro.local>
2016-10-03 15:25:31 -0400

    Implemented 2-3 tree besides insert, remove & rep_ok

--- a3/data.ml
+++ a3/data.ml
@@ -87,7 +87,74 @@ end
 
 (* TODO (later): implement a 2-3 tree.  For now,
    this code punts by equating the two functors. *)
-module MakeTreeDictionary = MakeListDictionary
+module MakeTreeDictionary (C : Comparable) = struct
+  module Key = C
+  type key = C.t
+
+  (* TODO: getting type ['value t] to something involving
+     association lists. *)
+  (* AF: TODO: document the abstraction function.
+   * RI: TODO: document any representation invariants. *)
+  type 'value t =
+    | Leaf
+    | Two_Node of 'value t * (key * 'value) * 'value t
+    | Three_Node of 'value t * (key * 'value)
+      * 'value t * (key * 'value) * 'value t
+
+
+  let rep_ok d =
+    raise Unimplemented
+
+  let empty = Leaf
+
+  let is_empty d = match d with
+  | Leaf -> true
+  | _ -> false
+
+  let rec size d = match d with
+  | Leaf -> 0
+  | Two_Node (w, (x,y), z) -> 1 + size w + size z
+  | Three_Node (t, (u,v), w, (x,y), z) -> 2 + size t + size w + size z
+
+  let remove k d =
+    raise Unimplemented
+
+  let rec insert k v d =
+
+    raise Unimplemented
+  (*match d with
+  | Leaf -> (Leaf, (k,v), Leaf)
+  | Two_Node (x, (k1,v1), y) -> if (C.compare k1 k = `EQ) then (x, (k,v), y) else
+    if (C.compare k k1 = `LT) then (insert k v x)*)
+
+  let rec find k d = match d with
+  | Leaf -> None
+  | Two_Node (w, (x,y), z) -> if (C.compare k x = `EQ) then Some y else if
+    (C.compare k x = `LT) then find k w else find k z
+  | Three_Node (t, (u,v), w, (x,y), z) -> if (C.compare k u = `EQ) then Some v else if
+    (C.compare k u = `LT) then find k t else if (C.compare k x =`EQ) then Some y else if
+    (C.compare k x = `LT) then find k w else find k z
+
+  let member k d = find k d <> None
+
+  let choose d = match d with
+  | Leaf -> None
+  | Two_Node (w, (x,y), z) -> Some (x,y)
+  | Three_Node (t, (u,v), w, (x,y), z) -> Some (u,v)
+
+  let rec to_list d = match d with
+  | Leaf -> []
+  | Two_Node (w, (x,y), z) -> (to_list w) @ (x,y)::(to_list z)
+  | Three_Node (t, (u,v), w, (x,y), z) -> (to_list t) @ (u,v)::(to_list w) @ (x,y)::(to_list z)
+
+
+  let fold f init d =
+    List.fold_left (fun acc (x,y) -> f x y acc) init (to_list d)
+
+  let format format_val fmt d =
+    Format.fprintf fmt "<abstr>" (* TODO: improve if you wish *)
+
+end
 
 module type Set =
   sig
