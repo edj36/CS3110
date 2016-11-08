bc16ddcd7e633a1336d6641cfd2d434781f5d172
Kenta Takatsu <kt426@cornell.edu>
2016-11-08 09:21:24 -0500

    intial

--- scrabble/ai.mli
+++ scrabble/ai.mli
@@ -2,4 +2,13 @@
 module AI : sig
   type state
   type hand
+
+  val serach_move : state -> move
+
+  val draw_letter : state -> state
+
+  val get_scote : state -> int
+
+  val submit_move : state -> move -> state
+
 end
