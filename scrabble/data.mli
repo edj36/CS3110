b98c256dcb0fa83b067efedce908f20d222810de
Kenta Takatsu <kt426@cornell.edu>
2016-11-08 00:22:01 -0500

    intial

---
+++ scrabble/data.mli
@@ -0,0 +1,33 @@
+type letter = { character: char; pt : int }
+
+type bonus_status =
+  | Double_letter
+  | Double_word
+  | Triple_letter
+  | Triple_word
+  | Center
+  | Normal
+
+type tile =
+{
+  coordinates : (int * int);
+  bonus : bonus_status;
+  isOccupied : bool;
+  char: char option
+}
+
+type scrabble_board = tile list list
+type score_board = (player * int) list
+type letter_bag = letter list
+type player_rack = letter list
+
+type game_state =
+{
+  board : scrabble_board;
+  score_board : score_board;
+  letter_bag : letter_bag;
+  player_racks : player_rack list;
+  turn : int
+}
+
+type scrabble_dictionary = string two_three_tree
--- scrabble/turn.mli
+++ scrabble/turn.mli
@@ -1,3 +1,9 @@
 module Turn : sig
 
+  type game_state
+  type player
+
+  val get_next : game_state -> player
+
+  val get_move : int -> string -> 
 end
