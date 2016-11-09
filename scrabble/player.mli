20497a4123fca46280391b72ce08a4296980f586
Kenta Takatsu <kt426@cornell.edu>
2016-11-09 15:40:36 -0500

    player.mli

--- scrabble/player.mli
+++ scrabble/player.mli
@@ -4,16 +4,27 @@
  *)
 
 module Player : sig
-
+  (* date type for game state *)
   type state
+  (* data type for the move player makes *)
   type move
 
-  val make_move : int -> int -> string -> move
+  (* make_move will return type move based on user input
+   * fist two [int] : the begining coordinate of word.
+   * [char] : C or R (column or row, specifiying the direction of the word)
+   * [string] : the word the user trys to submit*)
+  val make_move : int -> int -> char -> string -> move
 
+  (* draw_letter will be called when user decides to give up making a word and
+   * draw a letter from the letterbag *)
   val draw_letter : state -> state
 
+  (* get_score will be called after user types a word. it will return the
+   * new score after submitting the currrent move *)
   val get_scote : state -> int
 
+  (* submit_move will be called after confirming certain move by player.
+   * the move will be sent to the server and update the game state *)
   val submit_move : state -> move -> state
 
 
