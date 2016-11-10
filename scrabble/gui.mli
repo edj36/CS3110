7663b0e150fee4e74e256d86f97158aa256f4254
Kenta Takatsu <kt426@cornell.edu>
2016-11-09 23:54:29 -0500

    mli

--- scrabble/gui.mli
+++ scrabble/gui.mli
@@ -1,8 +1,8 @@
 (* Signature for GUI*)
 module GUI : sig
   (* data type for the game state *)
-	type state
+  type state
 
-	(* update will take the current game state and update GUI *)
-	val update : state -> unit
+  (* update will take the current game state and update GUI *)
+  val update : state -> unit
 end
