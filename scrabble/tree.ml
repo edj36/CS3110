3d39cba6a04cce390430fdfd62e082a0e9744a3c
Ubuntu <ericzhang211@gmail.com>
2016-11-28 18:22:06 -0500

    Successful Tree

--- scrabble/tree.ml
+++ scrabble/tree.ml
@@ -3,12 +3,18 @@ open PString
 exception Not_found
 
 let rec rec_read channel t =
+
+let mutabletree = ref (Node(MapChar.empty, [])) in
+
 try
+  while true; do
   let word = input_line (channel) in
-  let tree = PString.add word true t in
-  rec_read channel tree
+  let tree = PString.add word true !mutabletree in
+  mutabletree := tree
+done; !mutabletree
+
 with End_of_file ->
-close_in channel; t
+close_in channel; !mutabletree
 
 
 let text_read file =
@@ -20,3 +26,6 @@ let find_word str t =
  try
    find str t
  with _-> false
+
+
+let sd = text_read "dictionary.txt"
