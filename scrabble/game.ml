8192d600208854656660ecbe04f241a61d769875
Kenta Takatsu <kt426@cornell.edu>
2016-11-28 16:53:25 -0500

    gui setup

--- scrabble/game.ml
+++ scrabble/game.ml
@@ -2,6 +2,7 @@ open Data
 open Utils
 open State
 open Player
+open Gui
 
 (*********** INITIALIZE PLAYERS ***********)
 
@@ -17,47 +18,10 @@ let rec get_players input_string_list =
 
 
 
-(*********** GUI ***********)
-
-let print_board state =
-  print_string "\n";
-  let b = state.board in
-  for j = 0 to 14 do
-    for i = 0 to 14 do
-      let tile = get_tile (j,i) b in
-      let chr = match tile.letter with
-      | None -> " * "
-      | Some c -> " " ^ Char.escaped c ^ " " in
-      print_string chr;
-    done;
-    print_string "\n";
-  done
-
-
-let print_state state =
-  print_string ("Turn: " ^ (string_of_int state.turn) ^ "\n");
-  let player = current_player state in
-  let name = match fst player with
-  | Human n1 -> n1
-  | AI n2 -> n2 in
-  print_string (name ^ "\n");
-  let lst = List.map (fun x -> Char.escaped x.character) (snd player) in
-  let rec helper = function
-  | [] -> ""
-  | h::t -> h^ " " ^ helper t in
-  let hands = helper lst in
-  let () = print_string ("Player's hand: " ^ hands ^ "\n") in
-  let () = print_board state in
-  let () = List.fold_left (fun acc elm -> print_string (elm ^ "\n")) () state.words in
-  let () = print_string "\nnew words \n" in
-  let () = List.fold_left (fun acc elm -> print_string (elm ^ "\n")) () (get_newwords state)
-  in ()
-
-
 (*********** REPL ***********)
 
 let rec repl c_state =
-  let () = print_state c_state in
+  let () = update_gui c_state in
   let () = print_endline "\nEnter Move" in
   let s_move = read_line() in
   (* TODO AI vs Human input logic here *)
