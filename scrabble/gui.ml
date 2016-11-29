e0bbd2bc4d46b1231ecd8bbb5796252873bcf0b3
Kenta Takatsu <kt426@cornell.edu>
2016-11-29 01:56:58 -0500

    more debugging

--- scrabble/gui.ml
+++ scrabble/gui.ml
@@ -49,5 +49,6 @@ let update_gui state =
   let () = List.fold_left (fun acc elm -> print_string (elm ^ "\n")) () state.words in
   let () = print_string "\nnew words \n" in
   let () = List.fold_left (fun acc elm -> print_string (elm ^ "\n")) () (get_newwords (collect state.board) state.words) in
+  let () = print_string "\n" in
   let () = print_score state.score_board
   in ()
