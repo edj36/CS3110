faf42067ef6709536c5d5c76db10e5dd7129115e
edj36 <edj36@cornell.edu>
2016-12-03 02:54:49 -0500

    server can send and receive GET and POST

--- scrabble/scrabble_client.ml
+++ scrabble/scrabble_client.ml
@@ -2,16 +2,25 @@ open Lwt
 open Cohttp
 open Cohttp_lwt_unix
 
+(* https://github.com/mirage/ocaml-cohttp/tree/master/examples *)
+
 let get_state = 
-  Client.get (Uri.of_string "http://127.0.0.1:8000/") >>= fun (resp, body) -> 
+  Client.get (Uri.of_string "http://127.0.0.1:8080/") >>= fun (resp, body) -> 
   let code = resp |> Response.status |> Code.code_of_status in
-  Printf.printf "Response code: %d\n" code;
-  Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
+  Printf.printf "Response code: %d\n" code; 
   body |> Cohttp_lwt_body.to_string >|= fun body ->
   Printf.printf "Body of length: %d\n" (String.length body);
   body
 
+let post_move str = 
+  Client.post (Uri.of_string "http://127.0.0.1:8080/") >>= fun (resp, body) -> 
+  let code = resp |> Response.status |> Code.code_of_status in
+  Printf.printf "Response code: %d\n" code; 
+  body |> Cohttp_lwt_body.to_string >|= fun body ->
+  Printf.printf "Body of length: %d\n" (String.length body);
+  body
 
 let () =
-  let body = Lwt_main.run get_state in
-  print_endline ("Received body\n" ^ body)
+  let body = Lwt_main.run (post_move "D")  in
+  (*let body = Lwt_main.run (get_state) in *)
+  print_endline ("Received body:\n" ^ body)
--- scrabble/scrabble_server.ml
+++ scrabble/scrabble_server.ml
@@ -2,16 +2,26 @@ open Lwt
 open Cohttp
 open Cohttp_lwt_unix
 
+(* https://github.com/mirage/ocaml-cohttp/tree/master/examples *)
+
+let state_to_string s = s ^ "stringify"
+
 let server =
   let callback _conn req body =
     let uri = req |> Request.uri |> Uri.to_string in
-    let meth = req |> Request.meth |> Code.string_of_method in
     let headers = req |> Request.headers |> Header.to_string in
-    body |> Cohttp_lwt_body.to_string >|= (fun body ->
-      (Printf.sprintf "Uri: %s\nMethod: %s\nHeaders\nHeaders: %s\nBody: %s"
-         uri meth headers body))
-    >>= (fun body -> Server.respond_string ~status:`OK ~body ())
+    let meth = req |> Request.meth |> Code.string_of_method in
+    match req |> Request.meth with 
+    | `GET -> body |> Cohttp_lwt_body.to_string >|= (fun body ->
+        ("GET REQUEST RECEIVED\n" ^ body ^ "\n"))
+      >>= (fun body -> (Server.respond_string ~status:`OK ~body ()))
+    | `POST -> body |> Cohttp_lwt_body.to_string >|= (fun body ->
+        ("POST REQUEST RECEIVED\n" ^ body ^ "\n"))
+      >>= (fun body -> (Server.respond_string ~status:`OK ~body ()))
+    | _ -> body |> Cohttp_lwt_body.to_string >|= (fun body ->
+        ("ERROR\n"))
+      >>= (fun body -> (Server.respond_string ~status:`OK ~body ()))  
   in
-  Server.create ~mode:(`TCP (`Port 8000)) (Server.make ~callback ())
+  Server.create ~mode:(`TCP (`Port 8080)) (Server.make ~callback ())
 
 let () = ignore (Lwt_main.run server)
