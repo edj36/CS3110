22c7faeb7b52082fb1d250b0287bc056c0e3ef31
Ubuntu <ericzhang211@gmail.com>
2016-12-04 18:24:49 -0500

    AI Specs

--- scrabble/player.ml
+++ scrabble/player.ml
@@ -54,7 +54,8 @@ module Human : (Player with type t = string) = struct
 			Play
 			{
 				word = List.nth split 1;
-				direction = string_to_direction (String.lowercase_ascii (List.nth split 2));
+				direction = string_to_direction (String.lowercase_ascii
+          (List.nth split 2));
 				coordinate = coordinate
 			}
 			else raise Invalid
@@ -92,7 +93,8 @@ module AI : (Player with type t = Data.game_state) =  struct
 				if x = 14 then check_tile_board board (0, (y+1))
 					else check_tile_board board ((x+1), y)
 			| Some c ->
-				(* let () = print_int x; print_string " * "; print_int (y); print_endline "= coordinate found" in *)
+				(* let () = print_int x; print_string " * "; print_int (y);
+        print_endline "= coordinate found" in *)
 				(c, (x,y))
 
 	(*[space_check] is the number of empty tiles in a given [direction]
@@ -108,7 +110,9 @@ module AI : (Player with type t = Data.game_state) =  struct
 			if (tile.letter = None) then
 			1 + space_check (n_x, n_y) board direction else 0
 
-	(**)
+	(*[space_value] is a sequence of 4 ints (north, south, east, west)
+  that correspond with the number of empty tiles in that direction.
+  [Level] is an int that caps the number*)
 	let space_value level coordinate board =
 		let helper v = if v > level then level else v in
 		let north = helper (space_check coordinate board North)  in
@@ -129,17 +133,20 @@ module AI : (Player with type t = Data.game_state) =  struct
 	 let (player, rack_letters) = rack in
 		sublist rack_letters length
 
-	(* persmutaion of all the strings*)
+	(* [make_str] is a string list of single characters from a list of letters*)
 	let rec make_str letterlst = match letterlst with
 		| [] -> ""
 		| hd::tl -> (String.make 1 hd.character) ^ (make_str tl)
 
+ (*[remove] is a list [lst] without the first instance of [v]*)
   let rec remove v lst =
   match lst with
   | [] -> []
   | h::t -> if v = h then t else h :: remove v t
 
-	let rec permute letterlst =
+	(*[permute] is a list of all the permutations of the chracters in
+  [letterlst]*)
+  let rec permute letterlst =
 		match letterlst with
 		| [] -> []
 		| hd::[] -> [[hd]]
@@ -148,14 +155,20 @@ module AI : (Player with type t = Data.game_state) =  struct
 				acc @ List.map (fun l -> v::l) (permute (remove v letterlst)) in
 			List.fold_left helper [] letterlst
 
-	let str_permute letterlst =List.sort_uniq (compare) (List.map make_str (permute letterlst))
+	(*[str_permute] is a string list of all the permutations of [letterlst] sorted
+  and without any duplicates *)
+  let str_permute letterlst =List.sort_uniq (compare)
+  (List.map make_str (permute letterlst))
 
-	(*make a move check if its valid*)
+	(*[prepend] is a string list with a length [length] string prepended onto
+  character [ch] from a player's [rack] *)
 	let prepend ch length rack =
 		let letters = get_rack_letters rack length in
 		let prems = str_permute letters in
 		List.map (fun v -> v ^ (String.make 1 ch)) prems
 
+  (*[append] is a string list with a length [length] string appended onto
+  character [ch] from a player's [rack] *)
 	let append ch length rack =
 		let letters = get_rack_letters rack length in
 		let prems = str_permute letters in
@@ -163,29 +176,38 @@ module AI : (Player with type t = Data.game_state) =  struct
 
 	(* let is_valid m s = true *)
 
+
+  (*[char_to_int_brd] is the int corresponding to char [c] in a zero-indexed
+  board*)
 	let char_to_int_brd c = match c with
 	| 'a' -> 0 | 'b' -> 1 | 'c' -> 2 | 'd' -> 3 | 'e' -> 4 | 'f' -> 5 | 'g' -> 6
 	| 'h' -> 7 | 'i' -> 8 | 'j' -> 9 | 'k' -> 10 | 'l' -> 11 | 'm' -> 12
 	| 'n' -> 13 | 'o' -> 14 | _ -> failwith "out of bounds"
 
+  (*[int_to_char_brd] is the char corresponding to int [c] in a zero-indexed
+  board*)
 	let int_to_char_brd c = match c with
 	| 0 -> 'a' | 1 -> 'b' | 2 -> 'c' | 3 -> 'd' | 4 -> 'e' | 5 -> 'f' | 6 -> 'g'
 	| 7 -> 'h' | 8 -> 'i' | 9 -> 'j' | 10 -> 'k' | 11 -> 'l' | 12 -> 'm'
 	| 13 -> 'n' | 14 -> 'o' | _ -> failwith "out of bounds"
 
-	let print_play p = match p with
+	(*[print_play] prints the move [p] on the terminal*)
+  let print_play p = match p with
 	  | Play {
 	      word = str;
 	      direction = dir;
 	      coordinate = crd } ->
-	        let () = print_string (str ^ " was played by AI at ") in
-	        let () = print_char (fst crd); print_string " * "; print_int (snd crd); in
-	        if dir = Across then print_endline " across" else print_endline " down"
-	  | _ -> failwith "something bad"
+	   let () = print_string (str ^ " was played by AI at ") in
+	   let () = print_char (fst crd); print_string " * "; print_int (snd crd); in
+	     if dir = Across then print_endline " across" else print_endline " down"
+	  | _ -> failwith "invalid"
 
-	let ai_valid v state = try is_valid v state with _ -> false
+	(*[ai_valid] is a boolean of the validity of move [v] in state [state]*)
+  let ai_valid v state = try is_valid v state with _ -> false
 
-	let rec make_move w_lst dir st coord length rack c: Data.move =
+	(*[make_move] is the first valid move played in direction [dir] from a list
+  of valid moves generated from state [st]*)
+  let rec make_move w_lst dir st coord length rack c: Data.move =
 		let (n_dir, n_c) =
 		 match dir with
 			| North -> (Down, (fst coord - length, snd coord) )
@@ -210,8 +232,6 @@ module AI : (Player with type t = Data.game_state) =  struct
 				make_move w_l d st coord l rack c
 			(* | hd::tl -> print_play hd; hd *)
 			| hd::tl -> hd
-
-
 	and check_moves board sta (x,y) =
 		let rack = current_player_rack sta in
 		let level =
@@ -228,10 +248,12 @@ module AI : (Player with type t = Data.game_state) =  struct
 					(if (ea >= we) then ((append ch ea rack), East, ea)
 					else ((prepend ch we rack), West, we) )
 				else
-					(if (no >= so) then ((prepend ch no rack), North, no)
+					(if (no > so) then ((prepend ch no rack), North, no)
 					else ((append ch so rack), South, so) )in
 			make_move wo_lst dire sta co lth rack ch
 
+  (*[empty_move] is a valid move given a state [c_state] when the scrabble board
+  is empty*)
   let empty_move c_state =
     (* let board = c_state.board in *)
     let rack = current_player_rack c_state in
@@ -248,7 +270,9 @@ module AI : (Player with type t = Data.game_state) =  struct
 			(fun x -> try is_valid x c_state with _ -> false ) move_list in
        try List.hd valid_list with |_ -> SwitchAll
 
-	let execute_move s_state c_state =
+	(*[execute_move] is the new state after a valid move is played by the AI
+  given an initial state [s_state]*)
+  let execute_move s_state c_state =
 		let board = c_state.board in
     let move = if ((check_tile_board board (0,0)) = ('z',(15,15))) then
       empty_move c_state
